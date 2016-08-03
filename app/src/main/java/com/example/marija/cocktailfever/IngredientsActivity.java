dd38867c7075215fa9f72e367aa2fefd7618d51e
Andrea Mileska <andreamilesku@gmail.com>
2016-08-03 13:35:06 +0200

    Set screen orientation portait

--- app/src/main/java/com/example/marija/cocktailfever/IngredientsActivity.java
+++ app/src/main/java/com/example/marija/cocktailfever/IngredientsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +21,12 @@ import java.util.List;
 public class IngredientsActivity extends AppCompatActivity {
     private IngredientsListAdapter ingredientsAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+
 
         CocktailData.init();
 
--- app/src/main/java/com/example/marija/cocktailfever/zaednichka.java
+++
@@ -1,7 +0,0 @@
-package com.example.marija.cocktailfever;
-
-/**
- * Created by Marija on 6/7/2016.
- */
-public class zaednichka {
-}
